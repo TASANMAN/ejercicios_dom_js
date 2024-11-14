4c34cfb1e8ed4e87d98d17ec8956a37c362eb514
STRONG-EASY <maratarenzi@gmail.com>
2024-11-14 09:36:11 +0100

    Primer ejercicio

---
+++ Ejercicio1/ejercicio1.js
@@ -0,0 +1,25 @@
+// 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
+const showMeButton = document.querySelector('.showme')
+console.log(showMeButton)
+
+// 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
+const pilladoHeading = document.querySelector('#pillado')
+console.log(pilladoHeading)
+
+// 1.3 Usa querySelectorAll para mostrar por consola todos los <p>
+const paragraphs = document.querySelectorAll('p')
+console.log(paragraphs)
+
+// 1.4 Usa querySelectorAll para mostrar por consola todos los elementos con la clase .pokemon
+const pokemonElements = document.querySelectorAll('.pokemon')
+console.log(pokemonElements)
+
+// 1.5 Usa querySelectorAll para mostrar por consola todos los elementos con el atributo data-function="testMe"
+const testMeElements = document.querySelectorAll('[data-function="testMe"]')
+console.log(testMeElements)
+
+// 1.6 Usa querySelectorAll para mostrar por consola el 3er elemento con el atributo data-function="testMe"
+const thirdTestMeElement = document.querySelectorAll(
+  '[data-function="testMe"]'
+)[2]
+console.log(thirdTestMeElement)
